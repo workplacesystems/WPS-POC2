5554acafaa701d4fca8a1a62aa5d603e853c3066
thomash <thomash@THOMASH4530.workplace.com>
2014-01-24 10:42:56 +0000

    Test GIT commit 2

--- public/index.php
+++ public/index.php
@@ -1,5 +1,7 @@
 <?php
 
+echo "Hello world!";
+
 // Define path to application directory
 defined('APPLICATION_PATH')
     || define('APPLICATION_PATH', realpath(dirname(__FILE__) . '/../application'));
