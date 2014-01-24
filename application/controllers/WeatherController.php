a7e6101cb01591ead1c6159eaefe45335c9a8a6b
thomash <thomash@THOMASH4530.workplace.com>
2014-01-24 15:06:34 +0000

    First version of weather page

--- application/controllers/WeatherController.php
+++ application/controllers/WeatherController.php
@@ -2,37 +2,34 @@
 
 class WeatherController extends Zend_Controller_Action
 {
-    private $_WSDL_URI="http://192.168.188.128:8081/soap?wsdl";
-
-    public function init()
-    {
+    public function init() {
         /* Initialize action controller here */
     }
 
-    public function indexAction()
-    {
+    public function indexAction() {
         $request = $this->getRequest();
         $formData = $request->getPost();
         $postcode = $formData['postcode'];
         if($postcode) {
             
-            $wsdl_uri = 'http://api.worldweatheronline.com/free/v1/weather.ashx?q=' . $postcode . '&format=csv&num_of_days=5&key=3ujjqezksxanb8mkg85e9j83';
-            $ch = curl_init();
-            curl_setopt($ch, CURLOPT_URL, $wsdl_uri);
-            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-            $result = curl_exec($ch);
+            $weather = $this->getWeather($postcode);
 
-            $this->view->postcode = $postcode;
-            
-            $weatherRows = explode("\r\n", $result);
-            
-            // array containing the column headers for the data
+            $weatherRows = explode("\r\n", $weather);
             $weatherHeaders = explode(",", $weatherRows[6]);
             $weatherData = explode(",", $weatherRows[9]);
             
+            $this->view->postcode = $postcode;
             $this->view->weatherHeaders = $weatherHeaders;
             $this->view->weatherData = $weatherData;
         }
+    }
+    
+    private function getWeather($postcode) {
+        $wsdl_uri = 'http://api.worldweatheronline.com/free/v1/weather.ashx?q=' . $postcode . '&format=csv&num_of_days=5&key=3ujjqezksxanb8mkg85e9j83';
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $wsdl_uri);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        return curl_exec($ch);
     }
     
 }
