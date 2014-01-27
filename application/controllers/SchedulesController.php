5e9e3ed8d71397b695aa8706ba02c25b31217ce9
thomash <thomash@THOMASH4530.workplace.com>
2014-01-27 15:59:35 +0000

    Database connect test example 3

--- application/controllers/SchedulesController.php
+++ application/controllers/SchedulesController.php
@@ -7,7 +7,7 @@ class SchedulesController extends Zend_Controller_Action
     }
 
     public function indexAction() {
-        
+        $db = $this->connectDB();
         
         error_reporting(E_ALL);
         ini_set('display_errors', '1');
@@ -16,37 +16,11 @@ class SchedulesController extends Zend_Controller_Action
         $formData = $request->getPost();
         $schedule = new Schedule($formData['description'], $formData['start_date'], $formData['description']);
         if ($schedule->description) {
-           $schedule->save();
+           $schedule->save($db);
         }
         
-        $schedules = Schedule::getAll();
+        $schedules = Schedule::getAll($db);
         $this->view->schedules = $schedules;
-    }
-}
-
-/* Wouln;t normally shove it all in here, but it's just a POC! */
-class Schedule {
-
-    public $description;
-    public $start_date;
-    public $end_date;
-
-    function __construct($description, $start_date, $end_date) {
-        $this->description = $description;
-        $this->start_date = $start_date;
-        $this->end_date = $end_date;
-    }
-
-    function save() {
-        $db = $this->connectDB();
-        $db->insert(
-                'schedule', 
-                array(
-                    'description' => $this->description, 
-                    'start_date' => $this->start_date, 
-                    'end_date' => $this->end_date
-                )
-            );
     }
     
     function connectDB() {
@@ -64,10 +38,36 @@ class Schedule {
             )
         );
         return Zend_Db::factory($config->database);
-    }    
+    }        
+}
+
+/* Wouln;t normally shove it all in here, but it's just a POC! */
+class Schedule {
+
+    public $description;
+    public $start_date;
+    public $end_date;
+
+    function __construct($description, $start_date, $end_date) {
+        $this->description = $description;
+        $this->start_date = $start_date;
+        $this->end_date = $end_date;
+    }
+
+    function save($db) {
+        $db->insert(
+                'schedule', 
+                array(
+                    'description' => $this->description, 
+                    'start_date' => $this->start_date, 
+                    'end_date' => $this->end_date
+                )
+            );
+    }
     
-    static function getAll() {
-        $db = $this->connectDB();
+
+    
+    static function getAll($db) {
         $rows = $db->fetchAll("SELECT * FROM schedule");
         return $rows;
     }
